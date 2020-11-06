518206441b78935a188a74437485d6952cbb0d54
unizippro <mads@astrupa.dk>
2020-11-06 10:39:42 +0100

    Updating EDM, fixing parse, tests for parse, fix SwitchToEDM command, handling startup message

--- ublox-short-range/src/command/edm.rs
+++ ublox-short-range/src/command/edm.rs
@@ -18,6 +18,12 @@ pub(crate) const PAYLOAD_OVERHEAD: usize = 6;
 pub(crate) const AT_COMMAND_POSITION: usize = 5;
 /// Index in packet at which payload starts
 pub(crate) const PAYLOAD_POSITION: usize = 3;
+pub(crate) const STARTUPMESSAGE: &[u8] = b"\r\n+STARTUP\r\n";
+
+#[inline]
+pub(crate) fn calc_payload_len(resp: &[u8]) -> usize{
+    ((((resp[1] as u16) << 8) + resp[2] as u16) & EDM_FULL_SIZE_FILTER) as usize
+}
 
 #[derive(Debug, PartialEq)]
 #[repr(u8)]
@@ -81,11 +87,6 @@ impl From<u8> for PayloadType{
 pub struct EdmAtCmdWrapper<T>
 where
     T: AtatCmd,
-    <T as atat::AtatCmd>::CommandLen: core::ops::Add<EdmAtCmdOverhead>,
-    <<T as atat::AtatCmd>::CommandLen as core::ops::Add<EdmAtCmdOverhead>>::Output:
-        atat::heapless::ArrayLength<u8>,
-    <T as atat::AtatCmd>::Response: atat::serde_at::serde::Deserialize<'static>,
-    // <T as atat::AtatCmd>::Response: atat::serde_at::serde::Deserialize<'b>,
 {
     at_command: T,
 }
@@ -93,11 +94,6 @@ where
 impl <T> EdmAtCmdWrapper<T> 
 where
     T: AtatCmd,
-    <T as atat::AtatCmd>::CommandLen: core::ops::Add<EdmAtCmdOverhead>,
-    <<T as atat::AtatCmd>::CommandLen as core::ops::Add<EdmAtCmdOverhead>>::Output:
-        atat::heapless::ArrayLength<u8>,
-    <T as atat::AtatCmd>::Response: atat::serde_at::serde::Deserialize<'static>,
-    // <T as atat::AtatCmd>::Response: atat::serde_at::serde::Deserialize<'b>,
 {
     pub fn new(at_command: T) -> Self{
         EdmAtCmdWrapper{ at_command }
@@ -108,17 +104,13 @@ where
     }
 }
 
-// impl<'b, 'a: 'b, T> atat::AtatCmd for EdmAtCmdWrapper<'a, T>
 impl<T> atat::AtatCmd for EdmAtCmdWrapper<T>
 where
     T: AtatCmd,
     <T as atat::AtatCmd>::CommandLen: core::ops::Add<EdmAtCmdOverhead>,
     <<T as atat::AtatCmd>::CommandLen as core::ops::Add<EdmAtCmdOverhead>>::Output:
         atat::heapless::ArrayLength<u8>,
-    <T as atat::AtatCmd>::Response: atat::serde_at::serde::Deserialize<'static>,
-    // <T as atat::AtatCmd>::Response: atat::serde_at::serde::Deserialize<'b>,
 {
-    // type Response = EdmAtResponseWrapper<'a, T::Response>;
     type Response = T::Response;
     type CommandLen =
         <<T as atat::AtatCmd>::CommandLen as core::ops::Add<EdmAtCmdOverhead>>::Output;
@@ -142,24 +134,21 @@ where
         return s;
     }
 
-    // TODO: handle NoResponse in form of empty payload. Should do already
     fn parse(&self, resp: &[u8]) -> core::result::Result<Self::Response, atat::Error> {
         if resp.len() < PAYLOAD_OVERHEAD
             || !resp.starts_with(&[STARTBYTE])
             || !resp.ends_with(&[ENDBYTE])
         {
-            return Err(atat::Error::ParseString);
+            return Err(atat::Error::InvalidResponse);
         };
-        let payload_len = ((resp[1] as u16) << 8 + resp[2] as u16) & EDM_FULL_SIZE_FILTER;
-        if resp.len() != payload_len as usize + EDM_OVERHEAD
-            || resp[4] != PayloadType::ATConfirmation as u8
-        {
-            return Err(atat::Error::ParseString);
+        let payload_len = calc_payload_len(resp);
+        if resp.len() != payload_len + EDM_OVERHEAD
+            || resp[4] != PayloadType::ATConfirmation as u8 {
+            return Err(atat::Error::InvalidResponse);
         }
-        Err(atat::Error::Aborted)
-        // let at_resp = &resp[4..2 + payload_len as usize];
-        // atat::serde_at::from_slice::<Self::Response>(at_resp).map_err(|e| atat::Error::ParseString)
-
+        // Isolate the AT_response
+        let at_resp = &resp[AT_COMMAND_POSITION .. PAYLOAD_POSITION + payload_len];
+        self.at_command.parse(at_resp)
     }
 
     fn force_receive_state(&self) -> bool {
@@ -183,23 +172,40 @@ impl AtatUrc for EdmUrc{
 
     /// Parse the response into a `Self::Response` instance.
     fn parse(resp: &[u8]) -> Result<Self::Response, atat::Error>{
+        #[cfg(feature = "logging")]
+        log::info!("[Parse URC] {:?}", resp);
+        //Startup message?
+        if resp.windows(STARTUPMESSAGE.len()).position(|window| window == STARTUPMESSAGE) == Some(0){
+            return Ok(EdmUrc::StartUp);
+        }
+
         if resp.len() < PAYLOAD_OVERHEAD
             || !resp.starts_with(&[STARTBYTE])
-            || !resp.ends_with(&[ENDBYTE])
-        {
-            return Err(atat::Error::ParseString);
+            || !resp.ends_with(&[ENDBYTE]) {
+            #[cfg(feature = "logging")]
+            log::info!("[Parse URC Error] {:?}", resp);
+            return Err(atat::Error::InvalidResponse);
         };
-        let payload_len = (((resp[1] as u16) << 8) + resp[2] as u16) & EDM_FULL_SIZE_FILTER;
-        if resp.len() != payload_len as usize + EDM_OVERHEAD {
-            return Err(atat::Error::ParseString);
+        let payload_len = calc_payload_len(resp);
+        if resp.len() != payload_len + EDM_OVERHEAD {
+            #[cfg(feature = "logging")]
+            log::info!("[Parse URC Error] {:?}", resp);
+            return Err(atat::Error::InvalidResponse);
         }
 
         match resp[4].into() {
             PayloadType::ATEvent => {
-                Ok(EdmUrc::ATEvent(Urc::parse(&resp[AT_COMMAND_POSITION .. PAYLOAD_POSITION + payload_len as usize])?))
+                #[cfg(feature = "logging")]
+                log::info!("[Parse URC AT-CMD]: {:?}", &resp[AT_COMMAND_POSITION .. PAYLOAD_POSITION + payload_len]);
+                let cmd = Urc::parse(&resp[AT_COMMAND_POSITION .. PAYLOAD_POSITION + payload_len])?;
+                Ok(EdmUrc::ATEvent(cmd))
             }
             
-            _ => Err(atat::Error::ParseString)
+            _ => {
+                #[cfg(feature = "logging")]
+                log::info!("[Parse URC Error] {:?}", resp);
+                Err(atat::Error::InvalidResponse)
+            }
         }
     }
 }
@@ -214,9 +220,8 @@ impl atat::AtatCmd for SwitchToEdmCommand{
         return ChangeMode{mode: data_mode::types::Mode::ExtendedDataMode}.as_bytes();
     }
 
-    // TODO: handle NoResponse in form of empty payload. Should do already
     fn parse(&self, resp: &[u8]) -> core::result::Result<Self::Response, atat::Error> {
-        let correct = &[0xAAu8,0x00,0x06,0x00,0x45,0x4f,0x4b,0x0D,0x0a,0x55];
+        let correct = &[0xAAu8, 0x00, 0x02, 0x00, 0x71, 0x55];      // &[0xAAu8,0x00,0x06,0x00,0x45,0x4f,0x4b,0x0D,0x0a,0x55]; //AA 00 06 00 44 41 54 0D 0A 0D 0A 4F 4B 0D 0A 55 ?
         if resp.len() != correct.len() {
             return Err(atat::Error::InvalidResponse)
         } else if resp.windows(correct.len()).position(|window| window == correct) != Some(0){
@@ -234,13 +239,14 @@ impl atat::AtatCmd for SwitchToEdmCommand{
 #[cfg(test)]
 mod test {
     use super::*;
-    use atat::{AtatCmd, AtatLen, AtatResp, AtatUrc, heapless::{Vec, consts}};
+    use atat::{AtatCmd, AtatLen, AtatResp, AtatUrc, Error, heapless::{Vec, consts}};
     use crate::command::{
         Urc, 
         AT,
         system::{
             SystemStatus,
             types::StatusID,
+            responses::SystemStatusResponse,
         },
         data_mode::urc::PeerDisconnected,
     };
@@ -248,89 +254,89 @@ mod test {
     #[test]
     fn parse_at_commands(){
         let parse = EdmAtCmdWrapper::new(AT);
+        let correct_response = NoResponse;
 
-        // AT-command:"AT"
-        let correct = Vec::<u8, consts::U10>::from_slice(&[
-                0xAAu8,
-                0x00,
-                0x06,
-                0x00,
-                0x44,
-                0x41,
-                0x54,
-                0x0D,
-                0x0a,
-                0x55,
+        // AT-command: "AT"
+        let correct_cmd = Vec::<u8, consts::U10>::from_slice(&[
+                0xAAu8, 0x00, 0x06, 0x00, 0x44, 0x41, 0x54, 0x0D, 0x0a, 0x55,
             ]).unwrap();
-        assert_eq!(parse.as_bytes(), correct, "Parsing packet incorrect.\nExpectet: {:?}\nRecived:  {:?}", correct, parse.as_bytes());
+        // AT-response: NoResponse
+        let response = &[
+                0xAAu8, 0x00, 0x02, 0x00, PayloadType::ATConfirmation as u8, 0x55,
+            ];
+        assert_eq!(parse.as_bytes(), correct_cmd);
+        assert_eq!(parse.parse(response), Ok(correct_response));
         
-        let parse = EdmAtCmdWrapper::new(SystemStatus{status_id: Some(StatusID::SavedStatus)});
-
-        // AT-command:"at+umstat=1"
+        let parse = EdmAtCmdWrapper::new(SystemStatus{status_id: StatusID::SavedStatus});
+        let correct_response = SystemStatusResponse{status_id: StatusID::SavedStatus, status_val: 100};
+        // AT-command: "at+umstat=1"
         let correct = Vec::<u8, consts::U19>::from_slice(&[
-                0xAAu8,
-                0x00,
-                0x0F,
-                0x00,
-                0x44,
-                0x41, 
-                0x54, 
-                0x2b, 
-                0x55, 
-                0x4d, 
-                0x53, 
-                0x54, 
-                0x41, 
-                0x54, 
-                0x3d, 
-                0x31,
-                0x0D,
-                0x0A,
-                0x55,
+                0xAAu8, 0x00, 0x0F, 0x00, 0x44, 0x41, 0x54, 0x2b, 0x55, 0x4d, 0x53, 0x54, 0x41, 0x54, 0x3d, 0x31,0x0D,0x0A,0x55,
             ]).unwrap();
-        assert_eq!(parse.as_bytes(), correct, "Parsing packet incorrect.");
-       
+        // AT-response: "at+umstat:1,100"
+        let response = &[
+                0xAAu8,0x00,0x11,0x00,PayloadType::ATConfirmation as u8,0x2B,0x55, 0x4D, 0x53, 0x54, 0x41, 0x54, 0x3A, 0x31, 0x2C, 
+                0x31, 0x30, 0x30,0x0D,0x0A,0x55, 
+            ];
+        assert_eq!(parse.as_bytes(), correct);
+        assert_eq!(parse.parse(response), Ok(correct_response));
+    }
+
+    #[test]
+    fn parse_wrong_at_responses(){
+        let parse = EdmAtCmdWrapper::new(AT);
+        let correct_response = NoResponse;
+        // AT-response: NoResponse
+        let response = &[
+                0xAAu8, 0x00, 0x06, 0x00, PayloadType::ATConfirmation as u8, 0x55,
+            ];
+        assert_eq!(parse.parse(response), Err(Error::InvalidResponse), "Response shorter than indicated not invalid");
+
+        let parse = EdmAtCmdWrapper::new(SystemStatus{status_id: StatusID::SavedStatus});
+        // AT-response: "at+umstat:1,100"
+        let response =&[
+                0xAAu8,0x00,0x01,0x00,PayloadType::ATConfirmation as u8,0x2B,0x55, 0x4D, 0x53, 0x54, 0x41, 0x54, 0x3A, 0x31, 0x2C, 
+                0x31, 0x30, 0x30,0x0D,0x0A,0x55, 
+            ];
+        assert_eq!(parse.parse(response), Err(Error::InvalidResponse), "Response longer than indicated not invalid");
+
+        let response =&[
+                0xAAu8,0x00,0x11,0x00,PayloadType::ATConfirmation as u8,0x2B,0x55, 0x4D, 0x53, 0x54, 0x41, 0x54, 0x3A, 0x31, 0x2C, 
+                0x31, 0x30, 0x30,0x0D,0x0A,0x00, 
+            ];  
+        assert_eq!(parse.parse(response), Err(Error::InvalidResponse), "Response wrong endbyte not invalid");
+
+        let response =&[
+                0x00u8,0x00,0x11,0x00,PayloadType::ATConfirmation as u8,0x2B,0x55, 0x4D, 0x53, 0x54, 0x41, 0x54, 0x3A, 0x31, 0x2C, 
+                0x31, 0x30, 0x30,0x0D,0x0A,0x55, 
+            ];
+        assert_eq!(parse.parse(response), Err(Error::InvalidResponse), "Response wrong startbyte not invalid");
+
+        let response = &[
+                0xAAu8, 0x00, 0x02, 0x00, PayloadType::ATConfirmation as u8, 0x55,
+            ];
+        assert_eq!(parse.parse(response), Err(Error::ParseString), "Response wrong not invalid");
     }
 
     #[test]
     fn parse_urc(){
-        let resp = &[
-            0xAAu8,
-            0x00,
-            0x0C,
-            0x00,
-            0x41, //[4]
-            0x2B,
-            0x55,
-            0x55,
-            0x44,
-            0x50,
-            0x44,
-            0x3A,
-            0x33,
-            0x0D,
-            0x0A,
-            0x55,
+        // AT-urc: +UUDPD:3
+        let resp = &[ 
+            // 0xAAu8, 0x00, 0x0E, 0x00, PayloadType::ATEvent as u8, 0x0D, 0x0A, 0x2B, 0x55, 0x55, 0x44, 0x50, 0x44, 0x3A, 0x33, 0x0D, 0x0A, 0x55,
+            0xAAu8, 0x00, 0x0C, 0x00, PayloadType::ATEvent as u8, 0x2B, 0x55, 0x55, 0x44, 0x50, 0x44, 0x3A, 0x33, 0x0D, 0x0A, 0x55,
         ];
         let urc = EdmUrc::ATEvent(
             Urc::PeerDisconnected(PeerDisconnected{ handle: 3 })
         );    
-        let parsed_urc = EdmUrc::parse(resp).unwrap();
-        assert_eq!(parsed_urc, urc, "Parsing URC failed");
+        let parsed_urc = EdmUrc::parse(resp);
+        assert_eq!(parsed_urc, Ok(urc), "Parsing URC failed");
     }
 
     #[test]
     fn change_to_edm_cmd(){
-        let resp = &[0xAAu8,0x00,0x06,0x00,0x45,0x4f,0x4b,0x0D,0x0a,0x55];
-        let correct = Vec::<u8, <ChangeMode as atat::AtatCmd>::CommandLen>::from_slice(b"ATO=2\r\n").unwrap();
+        let resp = &[0xAAu8, 0x00, 0x02, 0x00, 0x71, 0x55];
+        let correct = Vec::<u8, <ChangeMode as atat::AtatCmd>::CommandLen>::from_slice(b"ATO2\r\n").unwrap();
         assert_eq!(SwitchToEdmCommand.as_bytes(), correct);
         assert_eq!(SwitchToEdmCommand.parse(resp).unwrap(), NoResponse);
     }
 }
-
-// #[derive(Clone)]
-// pub struct EdmAtResponseWrapper<'a, T> where T: AtatResp {
-//     pub at_resp: &'a T,
-// }
-
-// impl <'a, T> AtatResp for EdmAtResponseWrapper<'a, T> where T: AtatResp{}
