530b96d1affa4003912b4466a12887e7dd68658f
unizippro <mads@astrupa.dk>
2021-06-28 10:49:59 +0200

    fix linting

--- ublox-short-range/src/command/edm/mod.rs
+++ ublox-short-range/src/command/edm/mod.rs
@@ -242,9 +242,7 @@ impl atat::AtatCmd<6> for SwitchToEdmCommand {
         let resp = resp?;
         // Parse EDM startup command
         let correct = &[0xAAu8, 0x00, 0x02, 0x00, 0x71, 0x55]; // &[0xAAu8,0x00,0x06,0x00,0x45,0x4f,0x4b,0x0D,0x0a,0x55]; //AA 00 06 00 44 41 54 0D 0A 0D 0A 4F 4B 0D 0A 55 ?
-        if resp.len() != correct.len() {
-            return Err(atat::Error::InvalidResponse);
-        } else if resp
+        if resp.len() != correct.len() || resp
             .windows(correct.len())
             .position(|window| window == correct)
             != Some(0)
