c5219f7ba781de83095c6e6b957a263c5d0019e0
Mathias Koch <mk@blackbird.online>
2024-07-18 09:22:55 +0200

    Add missing defmt derive (#85)

--- src/network.rs
+++ src/network.rs
@@ -9,6 +9,7 @@ use heapless::String;
 use core::convert::TryFrom;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub enum WifiMode {
     Station,
     AccessPoint,
